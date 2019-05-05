8614294eadf490a164acdbf07d637feb0051d324
Arslan Ali <ali.arslan319@gmail.com>
2019-05-05 00:36:33 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,48 +1,10 @@
-# Make your shoe class here!
 class Shoe
+
+attr_accessor :properties, :shoe, :size, :color, :material, :condition
+attr_reader :brand
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
-  end
-
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
-  def cobble=(cobble)
-    @cobble = cobble
-  end
-
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
   end
 
   def cobble
